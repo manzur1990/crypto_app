e9999513e87d3d71743119a8c9c1adb64123916a
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2021-09-25 14:14:57 -0500

    added styling to App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ const App = () => {
             <div className='navbar'>
                 <Navbar />
             </div>
-            <div className='main'></div>
-            <div className='footer'></div>
+            <div className='main'>
+
+            </div>
+            <div className='footer'>
+                
+            </div>
         </div>
     )
 }
