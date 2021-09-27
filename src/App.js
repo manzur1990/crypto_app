8920e26848215bb410acc02b784c0fa550860d23
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2021-09-26 21:35:04 -0500

    added screen resize event handler in Navbar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
-import { Layout, Typography, Space} from 'antd'
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-// Components
-import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
-
-// Styling
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './styles/App.css'
 
-const App = () => {
-    return (
-        <div className= 'app'>
-            <div className='navbar'>
-                <Navbar />
-            </div>
-            <div className='main'>
-                <Layout>
-                    <div className='routes'>
-                        <Switch>
-                            <Route exact path='/'>
-                                <Homepage />
-                            </Route>
-                            <Route exact path='/exchanges'>
-                                <Exchanges />
-                            </Route>
-                            <Route exact path='/cryptocurrencies'>
-                                <Cryptocurrencies />
-                            </Route>
-                            <Route exact path='/crypto/:coinId'>
-                                <CryptoDetails />
-                            </Route>
-                            <Route exact path='/news'>
-                                <News />
-                            </Route>
-                        </Switch>
-                    </div>
-                </Layout>
-            </div>
-            <div className='footer'>
-
-            </div>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
         </div>
-    )
-}
-export default App
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
