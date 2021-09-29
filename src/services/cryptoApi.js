6e96175b93a5ab471f19b300fe9b82d29e07de47
JM <manzur-1990@hotmail.com>
2021-09-28 22:11:39 -0500

    added redux store initial files and setup

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'a3a62c81a3mshcec850ccdc40a4ep1bbe3djsn4ffe2dc3aefb'
+//     }
