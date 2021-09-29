0a1575b228bd48a178e2b7d04fbf237d251d018c
JM <manzur-1990@hotmail.com>
2021-09-28 22:30:33 -0500

    continue from previous commit

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'a3a62c81a3mshcec850ccdc40a4ep1bbe3djsn4ffe2dc3aefb'
+}
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': 'a3a62c81a3mshcec850ccdc40a4ep1bbe3djsn4ffe2dc3aefb'
-//     }
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats'
+
+const createRequest = (url) => ({ url, header: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder)=> ({
+        getCryptos: builder.query({
+            query: ()=> createRequest('/exchanges')
+        })
+    })
+})
+
