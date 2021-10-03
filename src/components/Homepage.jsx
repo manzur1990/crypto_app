09988ef885794108a4ec0fb35103a23d160a89d3
JM <manzur-1990@hotmail.com>
2021-10-03 11:29:23 -0500

    added Cryptocurrencies and News components

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -40,7 +40,16 @@ const Homepage = () => {
 
             <div className="home-heading-container">
                 <Title level={2} className='home-title'>Top 10 Cryptocurrencies</Title>
+                <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
             </div>
+
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className='home-title'>Latest Crypto News</Title>
+                <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
+            </div>
+            <News simplified />
+
         </>
     );
 };
